104e824b80ce91b431563569e464146d1b5ba93c
Aleksey <rusal@bk.ru>
2024-01-11 01:16:40 +0300

    ашч

--- quasar-project/src/pages/Index.vue
+++ quasar-project/src/pages/Index.vue
@@ -28,7 +28,7 @@
       :net-links="links"
       :options="options"
     ></d3-network></q-card-section>
-    ></q-card>
+    </q-card>
     <q-card class="chatmessages">
       <div class="flexrow">
         <div>
@@ -53,38 +53,66 @@
         </div>
       </div>
     </q-card>
-  </q-page>
   </div>
 </template>
 
 <script>
 import D3Network from "vue-d3-network";
 import { ref } from "vue";
+import axios from "axios";
+
 export default {
   components: {
     D3Network,
   },
   data() {
     return {
-      proc_text: ref(""),
+      proc_text: ref("черный орех"),
       optionsT: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
       threshold: ref(2),
+      hostae_uploadaem: ref("http://192.168.3.73:5000/uploadaem"),
       nodes: ref([
-        { id: 1, name: "" },
-        { id: 2, name: "" },
+        { id: 1, name: "a" },
+        { id: 2, name: "b" },
       ]),
       links: ref([
-        { sid: 1, tid: 2},
+        { sid: 1, tid: 2, name:'link 1'  },
         { sid: 2, tid: 1},
       ]),
       options: {
         force: 3000,
-        nodeSize: 15,
+        nodeSize: 10,
         nodeLabels: true,
+        linkLabels: true,
         linkWidth: 5,
       },
     };
   },
+  methods: {
+    fileUploadedM({ files, xhr }) {
+      let data = JSON.parse(xhr.response);
+      
+    },
+
+    async onProc() {
+      console.log(this.nodes)
+      console.log(this.links)
+      const response = await axios({
+        method: "post",
+        url: "http://192.168.3.73:5000/getdata",
+        data: {
+          text: this.proc_text,
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log('response');
+      console.log(response);
+      this.nodes = response.data.nodes;
+      this.links = response.data.links;
+    },
+  }
 };
 </script>
 <style lang="sass">
@@ -97,7 +125,7 @@ export default {
 .chatmessages
   padding: 10px
   margin: 10px
-.flexrowЁ
+.flexrow
   display: flex
   flex-flow: row wrap
   justify-content: flex-start
